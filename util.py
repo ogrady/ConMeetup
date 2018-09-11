a57cbae50ea351d4e41472b4221ff4f3bdb1a831
Daniel O'Grady <daniel.ogrady@uni-tuebingen.de>
2018-09-11 10:21:22 +0200

    Swap out logging functionality into util module

---
+++ util.py
@@ -0,0 +1,13 @@
+class Logger(object):
+    def __init__(self, filename, mode = "a"):
+        self._fh = open(filename, mode)
+
+    def error(self, message):
+        self._write("ERROR", message)
+
+    def close(self):
+        self._fh.close()
+
+    def _write(self, header, message):
+        ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        self._fh.write("[%s] %s: %s" % (header, ts, message))
